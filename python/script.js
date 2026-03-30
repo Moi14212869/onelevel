74a54e74749a8a89752f79d897f96698a77a41a4
Moi14212869 <eliott.1421@gmail.com>
2026-03-30 14:34:02 +0200

    Create script.js

---
+++ python/script.js
@@ -0,0 +1,9 @@
+function filterScripts() {
+  const input = document.getElementById("search").value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+    card.style.display = text.includes(input) ? "block" : "none";
+  });
+}
