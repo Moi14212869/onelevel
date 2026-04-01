7bee71abf86be12c62413fe50e4e72b05ce46d2a
OneLevel Studio <onelevel.studio.contact@gmail.com>
2026-04-01 20:48:56 +0200

    Update game.js

--- fish/game.js
+++ fish/game.js
@@ -4,36 +4,42 @@ const ctx = canvas.getContext('2d');
 const fish = {
     x: 100,
     y: 300,
-    width: 50,
-    height: 30,
+    radius: 20,
     speed: 5
 };
 
 const keys = {};
 
-// Écoute des touches du clavier
 window.addEventListener('keydown', (e) => { keys[e.key] = true; });
 window.addEventListener('keyup', (e) => { keys[e.key] = false; });
 
 function update() {
-    // Déplacement du poisson
-    if (keys['ArrowUp'] && fish.y > 0) fish.y -= fish.speed;
-    if (keys['ArrowDown'] && fish.y < canvas.height - fish.height) fish.y += fish.speed;
-    if (keys['ArrowLeft'] && fish.x > 0) fish.x -= fish.speed;
-    if (keys['ArrowRight'] && fish.x < canvas.width - fish.width) fish.x += fish.speed;
+    if (keys['ArrowUp'] && fish.y - fish.radius > 0) fish.y -= fish.speed;
+    if (keys['ArrowDown'] && fish.y + fish.radius < canvas.height) fish.y += fish.speed;
+    if (keys['ArrowLeft'] && fish.x - fish.radius > 0) fish.x -= fish.speed;
+    if (keys['ArrowRight'] && fish.x + fish.radius < canvas.width) fish.x += fish.speed;
+}
+
+function drawFish() {
+    // Corps (cercle)
+    ctx.fillStyle = 'orange';
+    ctx.beginPath();
+    ctx.arc(fish.x, fish.y, fish.radius, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Queue (triangle derrière)
+    ctx.fillStyle = 'darkorange';
+    ctx.beginPath();
+    ctx.moveTo(fish.x - fish.radius, fish.y);
+    ctx.lineTo(fish.x - fish.radius - 15, fish.y - 10);
+    ctx.lineTo(fish.x - fish.radius - 15, fish.y + 10);
+    ctx.closePath();
+    ctx.fill();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    // Dessiner le poisson (triangle simple)
-    ctx.fillStyle = 'orange';
-    ctx.beginPath();
-    ctx.moveTo(fish.x, fish.y);
-    ctx.lineTo(fish.x + fish.width, fish.y + fish.height / 2);
-    ctx.lineTo(fish.x, fish.y + fish.height);
-    ctx.closePath();
-    ctx.fill();
+    drawFish();
 }
 
 function gameLoop() {
